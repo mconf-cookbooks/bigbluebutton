c35081d7ba98bb016ba58f27ba050541a1a9f679
Felipe Cecagno <fcecagno@gmail.com>
2016-09-08 23:48:51 -0300

    cleanup

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,7 @@ maintainer_email "mconf@mconf.org"
 license          "MPL v2.0"
 description      "Installs a BigBlueButton server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.10"
+version          "2.0.0"
 supports         "ubuntu", "= 14.04"
 depends          "apt"
 depends          "ffmpeg"
