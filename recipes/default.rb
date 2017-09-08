89d23b4e32b2ad6ce88543e02d5d1da5d4d4344b
Felipe Cecagno <fcecagno@gmail.com>
2017-09-08 18:44:31 -0300

    configure new ppa repository for libssl 1.0.2

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,9 +22,13 @@ apt_repository "libreoffice" do
   key "libreoffice.key" # 1378B444
 end
 
-# add php repo for libssl 1.0.2 needed for chrome 52
 apt_repository "ondrej-php" do
   uri "ppa:ondrej/php"
+  distribution node['lsb']['codename']
+end
+
+apt_repository "openssl-backport" do
+  uri "ppa:ondrej/apache2"
   distribution node['lsb']['codename']
 end
 
