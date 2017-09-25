6c23aa2fa062d08a9be4705e086bf0253ca92504
Felipe Cecagno <fcecagno@gmail.com>
2017-09-25 00:35:58 -0300

    bump version

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,7 @@ maintainer_email "mconf@mconf.org"
 license          "MPL v2.0"
 description      "Installs a BigBlueButton server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.6"
+version          "2.0.7"
 supports         "ubuntu", "= 14.04"
 depends          "apt"
 depends          "ffmpeg"
